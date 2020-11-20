f6b38a2094b0d53ae7a7d2f3c8ee6249e2cedc98
Bouclette <anne.boucle@lilo.org>
2020-11-20 11:51:45 +0100

    Commit before weekend

--- src/Controller/FrontOfficeController.php
+++ src/Controller/FrontOfficeController.php
@@ -25,24 +25,12 @@ class FrontOfficeController extends AbstractController
             $recipes = $repo->findRandom();
             return $this->render('front_office/home.html.twig', [
                 'recipes' => $recipes,
-                'search_form' =>$searchForm->createView()
+                'search_form' => $searchForm->createView(),
+                'cards_title' => 'Les recettes à la Une'
                 ]);
         }
     }
 
-    public function createQuickSearchForm(Request $request) {
-        $recipe = new Recipe;
-        $searchForm = $this->createForm(QuickSearchType::class, $recipe);
-        return $searchForm->handleRequest($request);
-    }
-    
-    public function processQuickSearchForm($searchForm) {
-            $search = $searchForm->getData();
-            $search = $search->getName();
-            return $this->redirectToRoute('recipe_search', ['search' =>$search]);    
-    }
-
-        
     /**
      * @Route("/recipe/{id}", name="recipe_show")
      */
@@ -56,7 +44,7 @@ class FrontOfficeController extends AbstractController
 
         return $this->render('front_office/show.html.twig', [
             'recipe' => $recipe,
-            'search_form' => $searchForm->createView()
+            'search_form' => $searchForm->createView(),
             ]);
         }
     }
@@ -65,7 +53,6 @@ class FrontOfficeController extends AbstractController
      * @Route("/search/{search}", name="recipe_search")
      */
     public function search(RecipeRepository $repo, $search, PaginatorInterface $paginator, Request $request) {
-        $searchForm = $this->createQuickSearchForm($request);
 
         if($search != null) {
             $recipes = $paginator->paginate($repo->findByName($search));
@@ -73,11 +60,26 @@ class FrontOfficeController extends AbstractController
             $recipes = $paginator->paginate($repo->findAll());
         }
 
-    return $this->render('front_office/search.html.twig', [
-        'recipes' => $recipes,
-        'search_form' => $searchForm->createView()
-        ]);
+        return $this->render('front_office/search.html.twig', [
+            'recipes' => $recipes,
+            'cards_title' => "Résultats de votre recherche",
+            'search' => $search
+            ]);
     }
+
+    // Fonctions permettant de gérer le formulaire de recherche dans la navbar, à insérer dans chaque page
+    public function createQuickSearchForm(Request $request) {
+        $recipe = new Recipe;
+        $searchForm = $this->createForm(QuickSearchType::class, $recipe);
+        return $searchForm->handleRequest($request);
+    }
+    
+    public function processQuickSearchForm($searchForm) {
+            $search = $searchForm->getData();
+            $search = $search->getName();
+            return $this->redirectToRoute('recipe_search', ['search' =>$search]);    
+    }
+
 
 }
 
